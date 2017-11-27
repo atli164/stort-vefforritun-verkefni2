fc6fbab36f8d11e47aef52aa3f79ee34d9cfbc82
HjaltiDan <hjd1@hi.is>
2017-11-27 12:20:37 +0000

    Komment hreinsun á script.js
    Hreinsaði allar óþarfa umsagnir úr script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,81 +3,18 @@ class VideosMainPage {
     this.container = document.querySelector('body');
   }
 
-  // Einhverskonar startup fall
+  // Startup fall
   load() {
-    // Dæmi um hvernig mætti nota hrátt callback. Skil eftir ef ske kynni
-    // við vildum frekar nota það.
-    /*  const request = new XMLHttpRequest();
-      request.open('GET', 'videos.json', true);
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-
-          const data = JSON.parse(request.response);
-          console.log(data);
-        } else {
-          console.error('Villa', request);
-        }
-      };
-
-      request.onerror = function() {
-        console.error('Óþekkt villa');
-      };
-
-      request.send();
-    */
-
-    // Dæmi #2 um hvernig hægt er að ná í JSON gögn með asynchronous hætti,
-    // í þetta sinn með promises. Aðeins ítarlegra en það sem ég endaði á að
-    // nota, skil það eftir ef við endum á að vilja frekar nota þessa aðferð.
-    /*
-      var request = new Request('videos.json', { method: 'GET' });
-      fetch(request)
-        .then(function(response) {
-          if (response.status === 200) {
-            return response.json();
-          }
-          throw new Error('Something went wrong on api server!');
-        })
-        .then(function(response) {
-          // console.log(response);
-          return response;
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-     */
-
-
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      // Búum til private breytu sem geymir rétt scope á this...
-      // const that = this;
-      // console.log(that);
-      // ...sendum svo hana í onload og notum hana í JSON.parse
-      // xhr.onload = (that) => {
-      // Virkaði samt ekki án þess að kasta villu í npm. Notum lambda fall
-      // rétt á meðan við erum að klára aðra virkni.
       xhr.onload = function () {
         resolve(JSON.parse(this.response));
       };
       xhr.onerror = reject;
-
       xhr.open('GET', 'videos.json');
       xhr.send();
     });
   }
-
-  // Throwaway fall til að sýna hvernig við náum gögnum úr asynchronous
-  // promise til að vinna svo með gögnin
-  proofOfConcept(parsedResults) {
-    // console.log(parsedResults);
-    this.proofTwo(parsedResults);
-  }
-
-  // Annað throwaway til að sýna hvernig við hendum gögnum milli falla.
-  // proofTwo(parse) {
-  //  console.log(parse);
-  // }
 
   // Fall sem tekur við created tíma og spýtir út aldursstreng
   parseAge(created) {
@@ -86,11 +23,6 @@ class VideosMainPage {
     const year = 365 * 24 * 60 * 60 * 1000;
     if (now - date >= year) {
       const years = (now - date) / year;
-      // Kippti út years === 1 svo Math.floor(years) skili ekki áfram 1
-      // ef við erum í t.d. 1.9999.
-      // (Það er alveg til umræðu hvort við viljum vera að rúnna niður með
-      // Math.floor() yfirhöfuð; mér finnst það eðlilegast og tel það gera fyrir
-      // sem einfaldastan kóða, en við gætum farið aðrar leiðir)
       if ((years >= 1) && (years < 2)) {
         return 'Fyrir 1 ári síðan';
       }
@@ -137,14 +69,8 @@ class VideosMainPage {
 
   // Fall sem tekur við duration of spýtir út lengdarstreng
   parseDur(duration) {
-    // Mögulegt TODO: Bæta við klukkustundarreikningi og viðeigandi return skipun.
     const min = duration / 60;
     const sec = duration % 60;
-    // Já, við gætum gert þetta allt með
-    //  return min.toString().concat(':', sec.toString());
-    // en mér er illa við að keyra þrjú föll innan í return setningu - það
-    // leiðir til scope loss og undefined skemmtilegheita.
-    // Tek þetta frekar í sundur fyrir aðeins læsilegri og áreiðanlegri kóða.
     const minRounded = Math.floor(min);
     const minString = minRounded.toString();
     const secRounded = Math.floor(sec);
@@ -226,13 +152,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   videos.load()
     .then((result) => {
-      // videos.proofOfConcept(result);
-      // console.log(result);
       videos.parse(result);
     })
     .catch(() => {
       videos.displayLoadingerror();
-      // console.log("in catch");
-      // console.log(result);
     });
 });
